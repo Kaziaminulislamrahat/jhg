5acce4cf69ce3f3f7bad2debedc4e90e2533df7a
Kaziaminulislamrahat <info@kaziaminulislam.com>
2021-12-05 15:48:09 +0600

    status code add

---
+++ status code.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import requests
+df= pd.read_csv('D:\\New folder\\discover_not_index.csv')
+for url in df['urls']:
+   response= requests.get(url)
+   status= response.status_code
+   df['status']=status
+   df.to_csv('status_codes.csv')
+for url in df['urls']:
+  response= requests.get(url)
+  status= response.status_code
+  df['status']=status
+  if status == 404:
+    print(url)
